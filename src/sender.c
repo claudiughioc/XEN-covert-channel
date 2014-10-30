09c6df58bf3356ef284eae0ca0850c03bbb7775b
Claudiu Ghioc <claudiu.ghioc@virtualmetrix.com>
2014-10-30 13:16:32 +0200

    Separate common functions in utils
    Signed-off-by: Claudiu Ghioc <claudiu.ghioc@virtualmetrix.com>

--- src/sender.c
+++ src/sender.c
@@ -1,19 +1,7 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <sched.h>
-#include <errno.h>
-#include <string.h>
-#include <signal.h>
-#include <sys/time.h>
-#include <time.h>
-#include <unistd.h>
-
-#define LARGE_PRIME		492876847
-#define TIME_NSEC		50000000
-#define NSEC_TO_USEC(x)		((x) / 1000ULL)
+#include "utils.h"
 
 static int expired = 0;
-timer_t timer;
+static timer_t timer;
 
 static void timer_handler(int signal)
 {
@@ -21,42 +9,18 @@ static void timer_handler(int signal)
 	expired = 1;
 }
 
-static int start_timer(void)
-{
-	struct itimerspec its;
-
-	its.it_value.tv_sec = 0;
-	its.it_value.tv_nsec = TIME_NSEC;
-	its.it_interval.tv_sec = 0;
-	its.it_interval.tv_nsec = TIME_NSEC;
-	expired = 0;
-
-	return timer_settime(timer, 0, &its, NULL);
-}
-
-static int stop_timer(void)
-{
-	struct itimerspec its;
-
-	its.it_value.tv_sec = 0;
-	its.it_value.tv_nsec = 0;
-	expired = 0;
-
-	return timer_settime(timer, 0, &its, NULL);
-}
-
 static void send_one(void)
 {
 	unsigned long long x = 1;
 	printf("Sending one\n");
 
-	start_timer();
+	start_timer(&timer, &expired);
 
 	while (!expired) {
 		x *= LARGE_PRIME;
 	}
 
-	stop_timer();
+	stop_timer(&timer, &expired);
 	printf("Finished work\n");
 }
 
@@ -66,7 +30,6 @@ static void send_zero(void)
 	printf("Sending zero\n");
 	usleep(NSEC_TO_USEC(TIME_NSEC));
 }
-
 
 static void send(int val)
 {
@@ -82,49 +45,6 @@ static void send(int val)
 	}
 }
 
-
-static int set_afin(void)
-{
-	cpu_set_t mask;
-
-	CPU_ZERO(&mask);
-	CPU_SET(1, &mask);
-	return sched_setaffinity(0, sizeof(mask), &mask);
-}
-
-
-/* Initialize the sender
- * Set the signal handler and the timer */
-static int init_sender(void)
-{
-	int res = 0;
-	struct sigevent sevp;
-	struct sigaction sa;
-
-	/* Set process afinity */
-	if ((res = set_afin()))
-		printf("Error setting cpu affinity\n");
-
-	/* Set the sigalrm handler */
-	memset (&sa, 0, sizeof (sa));
-	sa.sa_handler = &timer_handler;
-	if ((res = sigaction(SIGALRM, &sa, NULL))) {
-		printf("Error setting the signal handler: %s\n", strerror(errno));
-		return res;
-	}
-
-
-	/* Create the timer */
-	sevp.sigev_notify = SIGEV_SIGNAL;
-	sevp.sigev_signo = SIGALRM;
-	if ((res = timer_create(CLOCK_MONOTONIC, &sevp, &timer))) {
-		printf("Error creating the timer %s\n", strerror(errno));
-		return res;
-	}
-
-	return res;
-}
-
 int main(void)
 {
 	int res = 0;
@@ -133,7 +53,7 @@ int main(void)
 
 	
 	/* Initialize the sender */
-	if ((res = init_sender()))
+	if ((res = init(timer_handler, &timer)))
 		return res;
 
 
